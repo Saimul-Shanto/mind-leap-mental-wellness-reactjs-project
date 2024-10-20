ca973bd8f68f9fdc9e1ff6437cbd5d88eecced61
Saimul-Shanto <saimulhaque@iut-dhaka.edu>
2024-10-20 17:28:13 +0600

    react, tailwind, assets setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold font-secondary underline text-red-400 text-center p-20'>hi</h1>
+    </>
+  )
+}
+
+export default App
